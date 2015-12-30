ad3a96bc63b54ca6e837f8d017f5c8c8a151c943
MickaelFonck <mickael_fonck@yahoo.fr>
2015-12-31 00:15:17 +0100

    Functional program

---
+++ MQ4_Detecteur_Gaz.ino
@@ -0,0 +1,35 @@
+#include <EEPROM.h>
+
+int val=0;
+unsigned long temps;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(8,OUTPUT);
+pinMode(13,OUTPUT);
+Serial.println("");
+Serial.println("Initialisation du MQ-4");
+temps=millis();
+}
+
+void loop() {
+  
+
+  if ((millis()-temps) < 5000) {
+    if (val > 100) {
+      digitalWrite(8,HIGH);
+      digitalWrite(13,HIGH);
+      //Serial.println("Détection haute");
+    } else if (val < 100) {
+      digitalWrite(8,LOW);
+      digitalWrite(13,LOW);
+      //Serial.println("Détection basse");
+    }
+  } else {
+    val=analogRead(A0);
+    EEPROM.write(0,val);
+    Serial.println(val);
+    temps=millis();
+  }
+  
+}
